4a4900753e34ade5a56b4299358ef04094a48070
Foreal <justinfsaintil@verizon.net>
2014-05-08 19:11:58 -0400

    Fixed Language and aesthetics

--- MadLib.py
+++ MadLib.py
@@ -6,18 +6,19 @@ class MadLibFrame(wx.Frame):
 		wx.Frame.__init__(self, parent, wx.ID_ANY, "Mad Lib Game")
 		self.panel = wx.Panel(self)
 		
-		self.btnClickMe = wx.Button(self.panel, label = "YES", pos = (20,80), size=(200,20))
-		self.btnClickMe2 = wx.Button(self.panel, label = "NO", pos = (20,120), size=(200,20))
-		heading = wx.StaticText(self.panel, label='Would You like to Start a Mad Lib Game?', pos=(20, 15))
+		self.btnClickMe = wx.Button(self.panel, label = "Start", pos = (20,80), size=(200,20))
+		self.btnClickMe2 = wx.Button(self.panel, label = "Cancel", pos = (20,120), size=(200,20))
+		heading = wx.StaticText(self.panel, label='Press Start to Begin.', pos=(20, 15))
+		heading2 = wx.StaticText(self.panel, label='Press Cancel to End.', pos=(20, 30))
 	
 		self.btnClickMe.Bind(wx.EVT_BUTTON, self.OnClickMe)
 		self.btnClickMe2.Bind(wx.EVT_BUTTON, self.OnClickMe2)
 
 	
 	def OnClickMe(self,e):
-		wx.MessageBox("LET THE GAMES BEGIN" ,"May the ODDS be forever in your favor", wx.CANCEL)
+		wx.MessageBox("You will be Choosing different words, for example: random, verbs, adjectives, and nouns.These words will then arranged creating sentences. " ,"Rules", wx.CANCEL)
 	def OnClickMe2(self,e):
-		wx.MessageBox("Now That's a let down" ,"Go Die in a hole", wx.CANCEL)
+		wx.MessageBox("There will be NO REFUNDS" ,"Cancel", wx.CANCEL)
 	
 
 app = wx.App(False) 
