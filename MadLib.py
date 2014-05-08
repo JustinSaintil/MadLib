0af2c68a4fc475b5359044d8175614a042fc8939
Foreal <justinfsaintil@verizon.net>
2014-05-08 19:44:16 -0400

    Got Rid of unneeded definitions and forced it into a module

--- MadLib.py
+++ MadLib.py
@@ -1,25 +1,9 @@
 #! /usr/bin/env python
 
-import wx 
-class MadLibFrame(wx.Frame):
-	def __init__(self, parent):
-		wx.Frame.__init__(self, parent, wx.ID_ANY, "Mad Lib Game")
-		self.panel = wx.Panel(self)
-		
-		self.btnClickMe = wx.Button(self.panel, label = "Start", pos = (20,80), size=(200,20))
-		self.btnClickMe2 = wx.Button(self.panel, label = "Cancel", pos = (20,120), size=(200,20))
-		heading = wx.StaticText(self.panel, label='Press Start to Begin.', pos=(20, 15))
-		heading2 = wx.StaticText(self.panel, label='Press Cancel to End.', pos=(20, 30))
-	
-		self.btnClickMe.Bind(wx.EVT_BUTTON, self.OnClickMe)
-		self.btnClickMe2.Bind(wx.EVT_BUTTON, self.OnClickMe2)
+import wx
+from buttonmodule import *
+# * imports everything in the module
 
-	
-	def OnClickMe(self,e):
-		wx.MessageBox("You will be Choosing different words, for example: random, verbs, adjectives, and nouns.These words will then arranged creating sentences. " ,"Rules", wx.CANCEL)
-	def OnClickMe2(self,e):
-		wx.MessageBox("There will be NO REFUNDS" ,"Cancel", wx.CANCEL)
-	
 
 app = wx.App(False) 
 frame = MadLibFrame(None)
