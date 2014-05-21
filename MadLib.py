ad4cd62f3ddba13f161e486afbb4237908ae773e
cloudcrest <apollo7@netzero.com>
2014-05-21 11:17:13 -0400

    Created variable names for random word txt files

--- MadLib.py
+++ MadLib.py
@@ -4,6 +4,13 @@ import wx
 from buttonmodule import *
 # * imports everything in the module
 
+vehicle = random.choice(vehicles)
+names = random.choice(names)
+objects = random.choice(objects)
+places = random.choice(places)
+verbs = random.choice(verbs)
+adj = random.choice(adj)
+
 
 app = wx.App(False) 
 frame = MadLibFrame(None)
